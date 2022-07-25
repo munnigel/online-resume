1f1202a2d6efb804de5da9eaf61d1ca0690f4b2a
munnigel <munnigel@yahoo.com.sg>
2022-07-25 13:28:14 +0800

    Changed header icon size and header padding-top

--- src/components/header/HeaderSocials.js
+++ src/components/header/HeaderSocials.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { IconContext } from 'react-icons'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
+      <IconContext.Provider value={{ size: '2rem' }}>
         <a href='https://www.linkedin.com/in/nigel-mun-25771a239/' target="_blank"><BsLinkedin /></a>
-        <a href='https://github.com/munnigel' target="_blank"><FaGithub /></a>
+        <a href='https://github.com/munnigel' target="_blank" ><FaGithub /></a>
+      </IconContext.Provider>
     </div>
   )
 }
